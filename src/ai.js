f5e861f9128292239930344297232033f8c0f829
whyismeleige <piyushjain31456@gmail.com>
2025-04-02 17:42:42 +0530

    first commit

--- src/ai.js
+++ src/ai.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "http://localhost:5000";
+const SERVER_URL = "https://sec-3.vercel.app";
 
 export async function getRecipeFromChefClaude(ingredientsArr) {
   const ingredientsString = ingredientsArr.join(",");
